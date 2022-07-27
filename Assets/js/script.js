026da2d2cb9d18e6bb828260b2e91dce14a4f9e7
Nilu <nirali.rkapadia@gmail.com>
2022-07-27 11:09:41 -0700

    Changed the event trigger from search to click to make the search functionality work

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -18,19 +18,6 @@ dateWeatherEl.text = dateW;
 const tomorrow = moment().add(1, 'days');
 console.log(tomorrow.format('YYYY-MM-DD'))
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '77c8fec0d0mshdb05a13cd8505e8p1c1508jsn238f4fdfc268',
-		'X-RapidAPI-Host': 'forecast9.p.rapidapi.com'
-	}
-};
-
-fetch('https://forecast9.p.rapidapi.com/rapidapi/forecast/Berlin/summary/', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
  
   var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -123,31 +110,7 @@ fetch('https://forecast9.p.rapidapi.com/rapidapi/forecast/Berlin/summary/', opti
       weatherContainerEl.append(wind);
       weatherContainerEl.append(humid);
       weatherContainerEl.append(uvIndex);
-    /*for (var i = 0; i < city.length; i++) {
-      var cityName = city[i].name.login + '/' + repos[i].name;
-  
-      var repoEl = document.createElement('a');
-      repoEl.classList = 'list-item flex-row justify-space-between align-center';
-      repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
-  
-      var titleEl = document.createElement('span');
-      titleEl.textContent = repoName;
-  
-      repoEl.appendChild(titleEl);
-  
-      var statusEl = document.createElement('span');
-      statusEl.classList = 'flex-row align-center';
-  
-      if (repos[i].open_issues_count > 0) {
-        statusEl.innerHTML =
-          "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-      } else {
-        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-      }
-  
-      repoEl.appendChild(statusEl);
-  
-      repoContainerEl.appendChild(repoEl);*/
+    
     };
-  userFormEl.addEventListener('search', formSubmitHandler);
+  userFormEl.addEventListener('click', formSubmitHandler);
   cityButtonsEl.addEventListener('click', buttonClickHandler);
